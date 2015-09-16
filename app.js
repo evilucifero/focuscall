ad4c0d9a61414fcd113c2c473900a27dc4813b4f
evilucifero <x@evilucifero.com>
2015-09-17 04:07:35 +0800

    2015 autumn update: add admin mod 519700

--- app.js
+++ app.js
@@ -4,43 +4,51 @@ var ejs = require('ejs');
 var path = require('path');
 var express = require('express');  
 var app = express();  
-var nownum = 0;
+var nowNum = 0;
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html',ejs.__express);  
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/',function(req, res){
-    res.render('show');
+  res.render('show');
 });
 app.get('/show', function(req, res){
-    res.render('show');
+  res.render('show');
 });
 app.get('/call', function(req, res){
-    res.render('call');
+  res.render('call');
 });
 app.get('/print', function(req, res){
-    res.render('print');
+  res.render('print');
 });
+app.get('/519700', function(req, res){
+  res.render('519700');
+});
+
 app.get('/api/requestcall', function(req, res){
-    var requestcall = {
-      call_num:nownum
-    };
-    res.send(JSON.stringify(requestcall)).end();
+  res.send(JSON.stringify({
+    callNum: nowNum
+  })).end();
 });
 app.get('/api/nextcall', function(req, res){
-    nownum++;
-    var nextcall = {
-      call_num:nownum
-    };    
-    res.send(JSON.stringify(nextcall)).end();
+  res.send(JSON.stringify({
+    callNum: ++nowNum
+  })).end();
 });
 app.get('/api/nowcall', function(req, res){
-    var nowcall = {
-      call_num:nownum
-    };    
-    res.send(JSON.stringify(nowcall)).end();
+  res.send(JSON.stringify({
+    callNum: nowNum
+  })).end();
 });
+app.get('/api/setcall', function(req, res){
+  nowNum = req.query.nowNum;
+  res.send(JSON.stringify({
+    callNum: nowNum
+  })).end();
+});
+
 app.listen(20205, function() {
-    console.log('Listening on port 20205.');
+  console.log('Listening on port 20205.');
 });
